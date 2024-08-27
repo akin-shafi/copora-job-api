12a8e519e2ffe1f7b29518db68d2d026795ff520
Shafi Akinropo <sakinropo@gmail.com>
2024-08-27 18:56:22 +0100

    update

--- src/controllers/OnboardingController.ts
+++ src/controllers/OnboardingController.ts
@@ -4,12 +4,12 @@ import { ApplicationService } from '../services/ApplicationService';
 export class OnboardingController {
     private applicationService = new ApplicationService();
 
-    async startOnboarding(req: Request, res: Response) {
-        try {
-            const application = await this.applicationService.createApplication();
-            res.status(200).send({ applicationNo: application.applicationNo });
-        } catch (error) {
-            res.status(500).send({ message: 'Error starting onboarding process', error });
-        }
-    }
+    // async startOnboarding(req: Request, res: Response) {
+    //     try {
+    //         const application = await this.applicationService.createApplication();
+    //         res.status(200).send({ applicationNo: application.applicationNo });
+    //     } catch (error) {
+    //         res.status(500).send({ message: 'Error starting onboarding process', error });
+    //     }
+    // }
 }
